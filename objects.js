88a3e322decd875e078832dc7b6917f032164f2c
Emerson04-sv <balmore04ebao@gmail.com>
2021-05-25 22:28:01 -0600

    Solo deje 3 atributos y los objetos los coloque dentro de un array

--- objects.js
+++ objects.js
@@ -2,19 +2,16 @@
 class pointList {
     /*Necesita de un constructor donde se pasan parametros*/
     constructor(food, points, isPoint){
-        this.food=food; /*this.=food es mi objeto=foot es el parametro*/
+        this.food=food; /*this.food: es el atributo so puedo poder this.comida o otra cosa. Y =food es el parametro, tiene que ser el mismo*/
         this.points=points;
         this.isPoint=isPoint;
-        this.info=`Es el boton ${this.food}, con ${this.points} de cal y es ${this.isPoint}`;
     } 
-    /*Esta funcion dentro de la clase, por lo que se llama metodo*/
-    verInfo(){
-        console.log(this.info);
-    }
-        
-};/*Esta es mi clase para hacer objetos*/
-/* */
-let burger = new pointList("hamburgesa",100, false);
-let iceCream = new pointList("helado", 200, false);
-let soda = new pointList("soda",300, false);
-let pizza = new pointList("pizza",400, false);
+    /*Cualquier funcion dentro de la clase, por lo que se llama metodo*/
+   
+}/*Esta es mi clase para hacer objetos*/
+
+const burger = new pointList("hamburgesa",100);
+const soda = new pointList("soda",300);
+const iceCream = new pointList("helado", 200);
+const pizza = new pointList("pizza",400);
+const obj=[burger,soda, iceCream, pizza];
