dbc4bb9c3d088dd222e32996f03efaae92629242
Emerson04-sv <balmore04ebao@gmail.com>
2021-05-25 22:29:12 -0600

    cambie los id por las clases y le asigno su valor por medio de una sola funcion

--- script.js
+++ script.js
@@ -1,87 +1,44 @@
-const btnDarkMode=document.getElementById("dark-mode");
-const btnBurger=document.getElementById("burger");
-const btnSoda=document.getElementById("soda");
-const btnPizza=document.getElementById("pizza");
-const btnIceCream=document.getElementById("ice-cream");
-const caloriasApp=document.getElementById("calorias-app")
+const allBtn=document.getElementsByClassName("all-btn")
+const txtCalPoints=document.getElementById("calorias-app")
 
-let cantDarkMode=false;
-let cantSelect=false;
-let points=[5,10,15,20];
-let a=0,b=0,c=0,d=0;
+let canDarkMode=false;
+let total=0;
 
-burger.verInfo();
-iceCream.verInfo();
-soda.verInfo();
-pizza.verInfo();
-
-btnBurger.addEventListener("click", function(){
-   assignBurgerPoints(points,cantSelect);
-});
-btnSoda.addEventListener("click", function(){
-    assignSodaPoints(points,cantSelect);
-});
-btnPizza.addEventListener("click", function(){
-    assignPizzaPoints(points,cantSelect);
-});
-btnIceCream.addEventListener("click", function(){
-    assignIceCreamPoints(points,cantSelect);
-});
-
-function assignBurgerPoints(points0, isntSelect){
-    let calBurger=0;
-    isntSelect=!isntSelect;
-    if(isntSelect)
-    {
-        calBurger=points0[0];
-    }
-    else
-    {   
-        calBurger=0;
-    }
-    console.log(calBurger);
+for(let i=0; i<allBtn.length; i++)
+{
+    allBtn[i].addEventListener("click", function(){
+        assignValue(obj[i]);
+    });
 }
 
-function assignIceCreamPoints(points1, isntSelect){
-    let calIceCream=0;
-    isntSelect=!isntSelect;
-    if(isntSelect)
+function assignValue(objeto)
+{
+    objeto.isPoint=!objeto.isPoint;
+    suma(objeto.isPoint, objeto.points);
+    styleBtn(objeto.isPoint);
+}
+
+function suma(isSelected, value)
+{
+    if(isSelected)
     {
-        calIceCream=points1[1];  
+        total+=value;
     }
     else
     {
-        calIceCream=0; 
+        total-=value;
     }
+    txtCalPoints.innerText=total;
 }
 
-function assignPizzaPoints(points2, isntSelect){
-    let calPizza=0;
-    isntSelect=!isntSelect;
-    if(isntSelect)
+function styleBtn(isSelected, canDarkMode){
+    canDarkMode=!canDarkMode;
+    if(isSelected && canDarkMode)
     {
-        calPizza=points2[2]; 
+        //Seleccionado y modo oscuro
     }
     else
     {
-        calPizza=0;    
+        //seleccionado 
     }
-}
-
-function assignSodaPoints(points3, isntSelect){
-    let calSoda=0;
-    isntSelect=!isntSelect;
-    if(isntSelect)
-    {
-        calSoda=points3[3]; 
-    }
-    else
-    {
-        calSoda=0;   
-    }
-}
-
-function totalPuntos(calPizza,calBurger,CalSoda,calIceCream){
-    let totalCal=calPizza+calBurger+CalSoda+calIceCream;
-    caloriasApp.innerText=totalCal;
 }
