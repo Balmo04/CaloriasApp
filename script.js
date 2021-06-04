57dd746d35d2a368217952a3aa61a984a5ec4f03
Emerson04-sv <balmore04ebao@gmail.com>
2021-06-03 21:59:46 -0600

    cambie el nombre de las clases

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /*Para el dark mode*/
 const header=document.getElementById("header");
-const headerTitles=document.getElementById("footer-titles");
+const headerTitles=document.getElementById("header-titles");
 const main=document.getElementById("main");
 const sectionFood=document.getElementById("section-food-text");
-const sectionTotal=document.getElementById("section-total-texto");
+const sectionTotal=document.getElementById("section-total-text");
+const transparency=document.getElementsByClassName("transparency");
 /*Funcionalidad*/
 const allBtn=document.getElementsByClassName("all-btn");
 const txtCalPoints=document.getElementById("calorie-accumulator");
@@ -23,7 +24,7 @@ function assignValue(object, i)
 {
     object.isValue=!object.isValue;
     suma(object.isValue, object.points); 
-    assignStyle(object.isValue, i)
+    assignStyle(object.isValue, i);
 }
 
 function suma(isSelected, points){
@@ -39,15 +40,15 @@ function suma(isSelected, points){
 function assignStyle(isSelected, position){
     if(isSelected){
         if(isDarkMode){
-            allBtn[position].classList.add("btn-section-style-dark-mode");
+            allBtn[position].classList.add("btn-dark-mode");
         }
         else{
-            allBtn[position].classList.add("btn-section-style-day-mode");
+            allBtn[position].classList.add("btn-light-mode");
         }       
     }
     else{
-        allBtn[position].classList.remove("btn-section-style-day-mode"); 
-        allBtn[position].classList.remove("btn-section-style-dark-mode");    
+        allBtn[position].classList.remove("btn-light-mode"); 
+        allBtn[position].classList.remove("btn-dark-mode");    
     }
 }
 
@@ -60,23 +61,25 @@ btnDarkMode.addEventListener("click", function(){
 function changeStylePage(){
     if(isDarkMode)
     {
-        header.classList.add("bg-blue");
+        btnDarkMode.classList.replace("bg-red", "bg-blue");
+        header.classList.replace("bg-red", "bg-blue");
         headerTitles.classList.add("txt-white");
-        main.classList.add("bg-gray");
+        main.classList.replace("bg-white","bg-gray");
         sectionFood.classList.add("txt-white");
         sectionTotal.classList.add("txt-white");
         for(i=0; i<allBtn.length; i++){
-            allBtn[i].classList.replace("btn-section-style-day-mode","btn-section-style-dark-mode")
+            allBtn[i].classList.replace("btn-light-mode","btn-dark-mode");       
         }
     }
     else{
-        header.classList.remove("bg-blue");
+        btnDarkMode.classList.replace("bg-blue", "bg-red");
+        header.classList.replace("bg-blue", "bg-red");
         headerTitles.classList.remove("txt-white");
-        main.classList.remove("bg-gray");
+        main.classList.replace("bg-gray","bg-white");
         sectionFood.classList.remove("txt-white");
-        sectionTotal.classList.remove("txt-white");  
+        sectionTotal.classList.remove("txt-white"); 
         for(i=0; i<allBtn.length; i++){
-            allBtn[i].classList.replace("btn-section-style-dark-mode","btn-section-style-day-mode")
+            allBtn[i].classList.replace("btn-dark-mode","btn-light-mode")
         }    
     }
 }
